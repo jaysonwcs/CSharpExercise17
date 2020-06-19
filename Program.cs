4782d27e7063dce0e35f96e8d33d0d60dfbdbb53
Jayson Silva <jaysonwcs@gmail.com>
2020-06-19 19:40:47 -0300

    Exercise finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CSharpExercise17.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace CSharpExercise17
 {
@@ -6,7 +11,47 @@ namespace CSharpExercise17
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string fullpath = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using StreamReader reader = File.OpenText(fullpath);
+                while (!reader.EndOfStream)
+                {
+                    string[] values = reader.ReadLine().Split(',');
+
+                    employees.Add(
+                        new Employee(values[0],
+                                values[1],
+                                double.Parse(values[2], CultureInfo.InvariantCulture)));
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has ocurred:");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            Console.Write("Enter salary: ");
+            double targetSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            IEnumerable<string> filteredEmails = employees
+                .Where(e => e.Salary > targetSalary)
+                .OrderBy(e => e.Email)
+                .Select(e => e.Email);
+                                           
+
+            Console.WriteLine("Email of people whose salary is more than " + targetSalary + ":");
+            foreach (var item in filteredEmails)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
