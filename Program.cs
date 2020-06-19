5824dd323316ad9a31efcadbb445c8dd45f3ec07
Jayson Silva <jaysonwcs@gmail.com>
2020-06-19 19:49:21 -0300

    Refactored

--- Program.cs
+++ Program.cs
@@ -9,11 +9,39 @@ namespace CSharpExercise17
 {
     class Program
     {
+        static string fullpath;
+        static double targetSalary;
+
         static void Main(string[] args)
         {
             Console.Write("Enter full file path: ");
-            string fullpath = Console.ReadLine();
+            fullpath = Console.ReadLine();
 
+            List<Employee> employees = GetEmployeesFromFile();
+
+            Console.Write("Enter salary: ");
+            targetSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            IEnumerable<string> filteredEmails = employees
+                .Where(e => e.Salary > targetSalary)
+                .OrderBy(e => e.Email)
+                .Select(e => e.Email);
+
+            DisplayResult(filteredEmails);
+        }
+
+        private static void DisplayResult(IEnumerable<string> filteredEmails)
+        {
+            Console.WriteLine("Email of people whose salary is more than " + targetSalary + ":");
+            foreach (var item in filteredEmails)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static List<Employee> GetEmployeesFromFile()
+        {
             List<Employee> employees = new List<Employee>();
 
             try
@@ -35,23 +63,7 @@ namespace CSharpExercise17
                 Console.WriteLine(e.Message);
             }
 
-
-
-            Console.Write("Enter salary: ");
-            double targetSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-
-            IEnumerable<string> filteredEmails = employees
-                .Where(e => e.Salary > targetSalary)
-                .OrderBy(e => e.Email)
-                .Select(e => e.Email);
-                                           
-
-            Console.WriteLine("Email of people whose salary is more than " + targetSalary + ":");
-            foreach (var item in filteredEmails)
-            {
-                Console.WriteLine(item);
-            }
+            return employees;
         }
     }
 }
